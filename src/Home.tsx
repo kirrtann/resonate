d1f0452d57a62b62b844e1b7d3bd1b16af5d0c12
kirrtann <sonagrakirtan@gmail.com>
2024-11-20 18:25:07 +0530

    card

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,31 @@
+import Card from "./common/Card";
+
 function Home() {
     return (
         <>
-            <div className=" bg-[#17312F] ">
+            <div className="pt-10  bg-[#17312F]  ">
                 <div className="w-full h-full  container flex justify-center items-center">
-                    <div className="bg-[url('download.jpeg')] bg-cover bg-center w-[70%] rounded-xl h-[500px]">
-                        <div className="text-white  bg-[#17312F] opacity-50 w-fit p-5 rounded-xl ">
-                            <div >  UI Desing</div>
-                            <h2 className="text-[30px]">Essential UI Design Tools for Designers</h2>
-                            <button className="hover:bg-[#CBFF54] border px-1 rounded-md border-[#CBFF54] text-[#CBFF54] hover:text-black ">read more</button>
+                    <div className="bg-[url('https://cdn.prod.website-files.com/66e9c11534c3c55b4c45a063/66e9deb71f04baae08259083_Hero.png')] flex relative justify-start items-end  bg-cover bg-center w-[80%] rounded-xl h-[500px]">
+                        <div className="text-white bottom-5 m-1 mb-6 backdrop-blur-sm bg-[#17312F]/50 w-fit p-5 rounded-xl ">
+                            <div>
+                                <p className="opacity-100">  UI Desing</p>
+                                <h2 className="text-[30px]">Essential UI Design Tools for Designers</h2>
+                                <button className="hover:bg-[#CBFF54] border px-1 rounded-md border-[#CBFF54] text-[#CBFF54] hover:text-black ">read more</button></div>
+                            <div className="justify-items-end"><img className="w-[30px] rounded-full  " src="https://cdn.prod.website-files.com/66e9c11534c3c55b4c45a063/66e9c11534c3c55b4c45a069_alex.png" alt="" /></div>
                         </div>
+
                     </div>
-                </div></div>
+                </div >
+                <div className="mt-10">
+                    <div><ul className="flex  rounded-md space-x-3 justify-center items-center p-4 sticky top-0  text-white  ">
+                        <li>All</li>
+                        <li>Ul DESING </li>
+                        <li>MARKETING</li>
+                        <li>SEO</li>
+                        <li>MOTOIN DESING</li></ul></div>
+                </div>
+                <div><Card /></div>
+            </div>
         </>
     );
 }
---
+++ src/common/Card.tsx
@@ -0,0 +1,26 @@
+function Card() {
+    return (
+        <>
+            <div className="">
+
+
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img className="rounded-t-lg" src="https://cdn.prod.website-files.com/66e9c11534c3c55b4c45a063/66e9dec00457dd623916ef40_Thumbnail.png" alt="" />
+                    </a>
+                    <div className="p-5">
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"> MOTION DESING</p>
+
+                        <a href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        </a>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+export default Card;

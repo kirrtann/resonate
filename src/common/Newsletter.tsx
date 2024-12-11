8463a00e6794c4a925a44bf878324faa511b1fb8
kirrtann <sonagrakirtan@gmail.com>
2024-12-11 14:14:07 +0530

    done without center blog

--- src/common/Newsletter.tsx
+++ src/common/Newsletter.tsx
@@ -1,11 +1,11 @@
 export function Newsletter() {
     return (
         <>
-            <div className=" container bg-[#17312F] max-w-[70rem] mr-auto p-2 ml-auto py-[100px]">
+            <div className=" container bg-[#17312F] max-w-[70rem] w-full mr-auto p-2 ml-auto py-[100px]">
                 <div className="sm:flex text-white bg-cover rounded-3xl 
                  font-mono bg-[url('https://cdn.prod.website-files.com/66e9c11534c3c55b4c45a058/66e9e3b926706c4ea7169531_Newsletter-BG.png')] bg-no-repeat ">
                     <div className="md:py-[7.5rem] md:pl-[5rem] pr-2 py-10 pl-10 ">
-                        <h6 className="text-[40px] pb-2 ">Subscribe to our newsletter</h6>
+                        <h6 className="sm:text-[40px] text-[25px] pb-2 ">Subscribe to our newsletter</h6>
                         <p className="pb-2">Sign up for your daily dose of creative inspiration, learnings, and growth.</p>
                         <form action="" className="space-y-2  ">
                             <input type="text " className="bg-transparent border border-gray-400 font-[0.75rem] rounded-md py-2 pl-5 w-[100%] outline-none placeholder:text-white hover:border-[#CBFF54] " placeholder="Email Address" /><br />
